bc006bec2c66e3258f6d3dc0dc929874d54873ef
chl <chl@aliquote.org>
2018-06-17 17:59:21 +0200

    Minor refactoring to fit with Spacemacs config
    Note: Files should now be ready to export with both org-html and ox-pandoc.
    For the latter, we should not specify any BST style after the BibTeX file,
    hence the `nil' value.

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@
 # 
 git log --format='* %h (%cD, %an) %s' | cat > history.txt
 
-gcp -u history.txt [0-9]*.{org,html} worg.css stata-logo.png fig-*.png ~/Sites/aliquote/content/articles/stata/
+gcp -u history.txt [0-9]*.{org,html} worg.css references.html stata-logo.png fig-*.png ~/Sites/aliquote/content/static/articles/stata/
