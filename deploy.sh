28a9880ace1c2b85616aac419586747cc37a9b38
chl <chl@aliquote.org>
2018-06-17 21:04:57 +0200

    Cleanup intermediate files

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-# Replace `gcp' with `cp' for true Un*x systems.
-# 
-git log --format='* %h (%cD, %an) %s' | cat > history.txt
-
-gcp -u history.txt [0-9]*.{org,html} worg.css references.html stata-logo.png fig-*.png ~/Sites/aliquote/content/static/articles/stata/
