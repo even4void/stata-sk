edda0b4af660223081381174ef673a2f7e78a30a
chl <chl@aliquote.org>
2018-06-18 15:07:35 +0200

    Fix missing def + Cosmit changes

--- setup.el
+++ setup.el
@@ -1,18 +1,32 @@
+;; FIXME Find a way to make the following independant of ELPA versioning
 (add-to-list 'load-path "~/.emacs.d/elpa/27.0/develop/org-plus-contrib-20180514")
-(add-to-list 'load-path "~/.emacs.d/elpa/27.0/develop/htmlize-20180412.1244/")
+(add-to-list 'load-path "~/.emacs.d/elpa/27.0/develop/htmlize-20180412.1244")
 (add-to-list 'load-path "~/.emacs.d/elpa/27.0/develop/ess-20180610.800/lisp")
+
+;; Required stuff
 (require 'ess-stata-mode)
 (require 'org)
 (load "ox-bibtex.el")
-(setq inferior-STA-program-name "/usr/local/bin/stata")
-(setq org-confirm-babel-evaluate nil
+
+;; Setup default settings
+(setq inferior-STA-program-name "/usr/local/bin/stata"
+      org-confirm-babel-evaluate nil
       org-src-fontify-natively nil
-      org-html-htmlize-output-type nil)
-(setq ess-ask-for-ess-directory nil)
+      org-html-htmlize-output-type nil
+      ess-ask-for-ess-directory nil)
 (org-babel-do-load-languages 'org-babel-load-languages '((stata . t)))
+
+;; Add-ons
+(org-add-link-type "span" #'ignore
+                   #'(lambda (class desc format)
+                       (pcase format
+                         (`html (format "<span class=\"%s\">%s</span>"
+                                        class
+                                        (or desc "")))
+                         (_ (or desc "")))))
 (org-add-link-type "stata" #'ignore #'my/export-stata-link)
 (defun my/export-stata-link (path desc format)
-  "Export org stata links to hmtl."
+  "Export org stata links to html."
   (cl-case format
     (html (format "<a href=\"https://www.stata.com/help.cgi?%s\" class=\"stata\">%s</a>" path (or desc "")))
     (latex (format "\"%s\")" path))))
