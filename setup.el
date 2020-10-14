9b419fe8ca2b17d59b740ec0f32e49853bda8a88
chl <chl@aliquote.org>
2020-10-14 17:39:24 +0200

    Work on chapter 4

--- setup.el
+++ setup.el
@@ -1,5 +1,6 @@
 ;; Now I use my whole Doom Emacs config instead of requiring
-;; individual pieces of Org modules one at a time.
+;; individual pieces of Org modules one at a time. (because
+;; jupyter-mode)
 (load (expand-file-name "init.el" user-emacs-directory))
 (require 'org)
 (load "ox-bibtex.el")
