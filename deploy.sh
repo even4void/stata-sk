79778c26efe494929c2d370f666e72302ce9c2f4
chl <chl@aliquote.org>
2022-05-12 10:48:06 +0200

    wip

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+# Note: Replace `gcp' and `gsed' with `cp' and `sed' for true Un*x systems.
+
+git log --format='* %h (%cD, %an) %s' | cat >history.txt
+
+if [ "$(uname)" == "Darwin" ]; then
+    gsed -i s+/Users/chl/org/drafts/_assets/++ dist/*.html
+    gcp -u history.txt dist/* ~/Sites/aliquote/static/articles/stata-sk/
+elif [ "$(uname -s)" == "Linux" ]; then
+    sed -i s+/home/chl/org/drafts/_assets/++ dist/*.html
+    cp -u history.txt dist/* ~/Sites/aliquote/static/articles/stata-sk/
+fi
