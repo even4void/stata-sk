13bd7fea086c9b623022205a62b0adaca6cb6729
chl <chl@aliquote.org>
2022-05-13 20:29:57 +0200

    Minor fix

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,6 @@
 #! /usr/bin/env bash
 
-# Note: Replace `gcp' and `gsed' with `cp' and `sed' for true Un*x systems.
-
 git log --format='* %h (%cD, %an) %s' | cat >history.txt
 
-if [ "$(uname)" == "Darwin" ]; then
-	gsed -i s+/Users/chl/org/drafts/_assets/++ dist/*.html
-	gcp -u history.txt dist/* ~/Sites/aliquote/static/articles/stata-sk/
-elif [ "$(uname -s)" == "Linux" ]; then
-	cp -u history.txt dist/* ~/Sites/aliquote/static/articles/stata-sk/
-fi
+cp -r -u history.txt dist/* ~/Sites/aliquote/static/articles/stata-sk/
+rm ~/Sites/aliquote/static/articles/stata-sk/fig-*.eps
